eccaaa709d3e0850a40de557cf2898b35ebc187a
yuseiatlas <rasengan40@gmail.com>
2021-12-21 09:01:48 +0100

    Removes unused injection in MainActivity

--- app/src/main/java/com/example/androidsample/ui/MainActivity.kt
+++ app/src/main/java/com/example/androidsample/ui/MainActivity.kt
@@ -2,16 +2,11 @@ package com.example.androidsample.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.apollographql.apollo3.ApolloClient
 import com.example.androidsample.R
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
-    @Inject
-    lateinit var apolloClient: ApolloClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
